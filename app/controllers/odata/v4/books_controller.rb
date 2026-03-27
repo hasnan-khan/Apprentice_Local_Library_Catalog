8337bfcb88e72021136b0bb9358f51fee95f84be
JeremyC-nava <jeremyclark@navapbc.com>
2026-03-27 11:43:34 -0400

    Corrected query syntax which should fix CI/CD Failure. Added CRUD for @book

--- app/controllers/odata/v4/books_controller.rb
+++ app/controllers/odata/v4/books_controller.rb
@@ -4,7 +4,7 @@ class Odata::V4::BooksController < ApplicationController
     render json: @books
   end
 
-  def show_detailed_exceptions?
+  def show
     @book = Book.find(params[:id])
 
     if @book
@@ -12,5 +12,39 @@ class Odata::V4::BooksController < ApplicationController
     else
       render json: { error: "Book not found" }, status: :not_found
     end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    if @book&.update(book_params)
+      render json: @book
+    else
+      render json: { error: @book ? @book.errors : "Book not found" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    if @book
+      @book.destroy
+      head :no_content
+    else
+      render json: { error: "Book not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :short_description)
   end
 end
